712855ddace55c934a05bac94d665e85798d7fc7
Daniel Wood <daniel.j.wood11@gmail.com>
2018-02-07 17:34:02 -0500

    add js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function IS_DESKTOP(){
+	return d3.select("#isDesktop").style("display") == "block";
+}
+function IS_TABLET(){
+	return d3.select("#isTablet").style("display") == "block";
+}
+function IS_PHONE(){
+	return d3.select("#isPhone").style("display") == "block";
+}
