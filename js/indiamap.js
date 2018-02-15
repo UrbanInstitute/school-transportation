cedfe8ffafc1d3f65e4452a4cc8d85406d182b0f
Daniel Wood <daniel.j.wood11@gmail.com>
2018-02-15 18:20:19 -0500

    add everything from india map

---
+++ js/indiamap.js
@@ -0,0 +1,54 @@
+function IS_DESKTOP(){
+	return d3.select("#isDesktop").style("display") == "block";
+}
+function IS_TABLET(){
+	return d3.select("#isTablet").style("display") == "block";
+}
+function IS_PHONE(){
+	return d3.select("#isPhone").style("display") == "block";
+}
+	
+d3.json("data/wards.geojson", function(err, data) {
+	console.log(data)
+	// var dataIndex = d3.map(data.features, function(d) { return d.properties.WARD; });
+	// console.log(dataIndex.$7)
+    mapDraw(data);
+});
+
+function mapDraw(geojson) {
+	
+	mapboxgl.accessToken = 'pk.eyJ1IjoidXJiYW5pbnN0aXR1dGUiLCJhIjoiTEJUbmNDcyJ9.mbuZTy4hI_PWXw3C3UFbDQ';
+	var map = new mapboxgl.Map({
+	  container: 'map', 
+	  style: 'mapbox://styles/urbaninstitute/cjdozdvbd02lv2sswwwuxsxmr', 
+	  center: [-77.0265709, 38.8970754], 
+	  zoom: 9,
+	  interactive: false
+	});
+
+    var container = map.getCanvasContainer()
+    var svg = d3.select(container).append("svg")
+
+    var transform = d3.geoTransform({point: projectPoint});
+	var path = d3.geoPath().projection(transform);
+
+	var featureElement = svg.selectAll("path")
+		.data(geojson.features.filter(function(d){return d.properties.WARD === 7}))
+		.enter()
+		.append("path")
+    
+    function update() {
+        featureElement.attr("d",path);
+    }
+
+    update()
+
+
+	function projectPoint(lon, lat) {		
+        var point = map.project(new mapboxgl.LngLat(lon, lat));
+		this.stream.point(point.x, point.y);
+	}
+
+}
+
+  
